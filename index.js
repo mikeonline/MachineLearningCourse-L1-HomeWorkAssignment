92bacdb0e2974276714dd02d420349f5a8061aa2
Mike Weaver <mikey.weaver@gmail.com>
2019-08-31 22:37:46 +0800

    Added Custom Classifier
    Added Custom Classifier

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
 
 let net;
 
@@ -9,12 +10,25 @@ async function app() {
   	console.log('Sucessfully loaded model');
   	// call to the webcam
 	await setupWebcam();
+	const addExample = classId => {
+		const activation = net.infer(webcamElement, 'cov_preds');
+		classifier.addExample(activation,classId);
+	};
+
+	document.getElementById('class-a').addEventListener('click', () => addExample(0));
+	document.getElementById('class-b').addEventListener('click', () => addExample(1));
+	document.getElementById('class-c').addEventListener('click', () => addExample(2));			
+
 	while (true) {
-		const result = await net.classify(webcamElement);
-		document.getElementById('console').innerText = `
-		prediction: ${result[0].className}\n
-		prediction: ${result[0].probability}
-		`;
+		if (classifier.getNumClasses() > 0) {
+			const activation = net.infer(webcamElement, 'cov_preds');
+			const result = await classifier.predictClass(activation);
+			const classes = ['A','B','C'];
+			document.getElementById('console').innerText = `
+			prediction: ${classes[result.classIndex]}\n
+			prediction: ${result.confidences[result.classIndex]}
+			`;			
+		}
 		// TensorFlow: Returns a promise that resolve when a requestAnimationFrame has completed.
 		await tf.nextFrame();
 	}
